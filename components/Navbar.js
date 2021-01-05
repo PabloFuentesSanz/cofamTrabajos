5903da5e49f19ae01ee206fec5e9c4d0a18eae1a
Pablo Fuentes <fuentessanzpablo@gmail.com>
2021-01-06 00:11:02 +0100

    Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import styles from "../styles/Header.module.css";
+import styles from "../styles/Navbar.module.css";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { logout } from "../firebase/client";
 
@@ -9,16 +9,16 @@ export default function Header() {
   };
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Tejados Cofam</Navbar.Brand>
+      <Navbar.Brand href="#home"> Tejados Cofam</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="">Inicio</Nav.Link>
           <Nav.Link href="">Control de Trabajo</Nav.Link>
           <Nav.Link href="">Obras Realizadas</Nav.Link>
-          <Nav.Link href="">Trabajadores</Nav.Link>
+          <Nav.Link  href="">Trabajadores</Nav.Link>
           <NavDropdown.Divider />
-          <Nav.Link onClick={handleClick}>Cerrar Sesión</Nav.Link>
+          <Nav.Link className={styles.cerrar} onClick={handleClick}>Cerrar Sesión</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
